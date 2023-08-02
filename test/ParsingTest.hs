e80d224daa11e22048896e2c029ac646777bbd15
kintrix007 <boldizsar02@gmail.com>
2023-08-02 10:15:22 +0200

    test: Add additional test to ensure curried function calls get parsed as well

--- test/ParsingTest.hs
+++ test/ParsingTest.hs
@@ -11,7 +11,7 @@ tests = TestList
   , tc "int2" $ assertBool "holds" ("(6)" `parseTo` EInt 6)
   , tc "int3" $ assertBool "holds" ("(-2)" `parseTo` EInt (-2))
   , tc "int4" $ assertBool "holds" ("-1024" `parseTo` EInt (-1024))
-  , tc "notInt" $ assertBool "holds" ("(- 2)" `parseTo` ECall "-" [EInt 2])
+  , tc "notInt" $ assertBool "holds" ("(- 2)" `parseTo` ECall (EVar "-") [EInt 2])
   , tc "var1" $ assertBool "holds" ("x" `parseTo` EVar "x")
   , tc "var2" $ assertBool "holds" ("<=>" `parseTo` EVar "<=>")
   , tc "var3" $ assertBool "holds" ("x+1=2" `parseTo` EVar "x+1=2")
@@ -19,14 +19,16 @@ tests = TestList
     `parseTo` EVar "This has spaces")
   , tc "var5" $ assertBool "holds" ("(foo|bar)" `parseTo` EVar "foo|bar")
   , tc "call1" $ assertBool "holds" ("(f x y)"
-    `parseTo` ECall "f" [EVar "x", EVar "y"])
+    `parseTo` ECall (EVar "f") [EVar "x", EVar "y"])
   , tc "call2" $ assertBool "holds" ("(foo (g x) (h 1 y))"
-    `parseTo` ECall "foo" [ECall "g" [EVar "x"], ECall "h" [EInt 1, EVar "y"]])
+    `parseTo` ECall (EVar "foo") [ECall (EVar "g") [EVar "x"], ECall (EVar "h") [EInt 1, EVar "y"]])
+  , tc "call2" $ assertBool "holds" ("(((foo) 5) 4)"
+    `parseTo` ECall (ECall (EVar "foo") [EInt 5]) [EInt 4])
   , tc "notVar" $ assertBool "fails" (fails "1+1=2")
   , tc "if1" $ assertBool "holds" ("(if 1 2 3)"
     `parseTo` EIf (EInt 1) (EInt 2) (EInt 3))
   , tc "if2" $ assertBool "holds" ("(if (> 42 0) (1) (0))"
-    `parseTo` EIf (ECall ">" [EInt 42, EInt 0]) (EInt 1) (EInt 0))
+    `parseTo` EIf (ECall (EVar ">") [EInt 42, EInt 0]) (EInt 1) (EInt 0))
   , tc "ifFails1" $ assertBool "fails" (fails "(if (= x 1))")
   , tc "ifFails2" $ assertBool "fails" (fails "(if (= x 1) (print 42))")
   , tc "let1" $ assertBool "holds" ("(let ((a 5))(a))"
@@ -48,9 +50,9 @@ tests = TestList
     `parseTo` ELetRec [("x", EInt 1)] (ELetRec [("y", EVar "x")] (EVar "y")))
   , tc "letrecFails1" $ assertBool "fails" (fails "(letrec () (5))")
   , tc "func1" $ assertBool "holds" ("(fn (x y) (+ x y))"
-    `parseTo` EFunc ["x", "y"] (ECall "+" [EVar "x", EVar "y"]))
+    `parseTo` EFunc ["x", "y"] (ECall (EVar "+") [EVar "x", EVar "y"]))
   , tc "func2" $ assertBool "holds" ("(fn (|foo bar|) (* |foo bar| 2))"
-    `parseTo` EFunc ["foo bar"] (ECall "*" [EVar "foo bar", EInt 2]))
+    `parseTo` EFunc ["foo bar"] (ECall (EVar "*") [EVar "foo bar", EInt 2]))
   , tc "func3" $ assertBool "holds" ("(fn (x) x)"
     `parseTo` EFunc ["x"] (EVar "x"))
   , tc "funcFails1" $ assertBool "holds" (fails "(fn () (10))")
@@ -58,10 +60,10 @@ tests = TestList
   , tc "funcFails3" $ assertBool "holds" (fails "(fn (10))")
   , tc "whitespace1" $ assertBool "holds"
     ("let((x y)(a b))(x(a))"
-    `parseTo` ELet [("x", EVar "y"), ("a", EVar "b")] (ECall "x" [EVar "a"]))
+    `parseTo` ELet [("x", EVar "y"), ("a", EVar "b")] (ECall (EVar "x") [EVar "a"]))
   , tc "whitespace2" $ assertBool "holds"
     (" ( let  (  ( x  y   ) (  a b ) )   ( ( x )a ) ) "
-    `parseTo` ELet [("x", EVar "y"), ("a", EVar "b")] (ECall "x" [EVar "a"]))
+    `parseTo` ELet [("x", EVar "y"), ("a", EVar "b")] (ECall (EVar "x") [EVar "a"]))
   , tc "whitespace3" $ assertBool "holds"
     ("(fn(x)(if(x)1 2))"
     `parseTo` EFunc ["x"] (EIf (EVar "x") (EInt 1) (EInt 2)))
