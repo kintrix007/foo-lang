41ddc429fc926df525a40b1c38d2113ef49d1cbb
kintrix007 <boldizsar02@gmail.com>
2023-08-02 11:48:44 +0200

    fix: Remove outdated comment

--- src/Expression.hs
+++ src/Expression.hs
@@ -2,8 +2,6 @@ module Expression (Expression (..), Ident) where
 
 type Ident = String
 
--- Not 100% sure if debug can be implemented
--- as a function..? Hopefully yes.
 data Expression
   = EInt !Integer
   | EVar !Ident
