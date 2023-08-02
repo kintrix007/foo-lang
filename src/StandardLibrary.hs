39ce00d484528a3249d1d5f5b5145717a6e7816d
kintrix007 <boldizsar02@gmail.com>
2023-08-02 11:55:14 +0200

    feat: Add 'print' to the standard library

--- src/StandardLibrary.hs
+++ src/StandardLibrary.hs
@@ -1,7 +1,8 @@
 module StandardLibrary (builtins) where
 
-import qualified Data.Map as M
-import           Value    (Environment, Value (..))
+import qualified Data.Map    as M
+import           Debug.Trace (trace)
+import           Value       (Environment, Value (..))
 
 builtins :: Environment
 builtins = M.fromList
@@ -9,6 +10,7 @@ builtins = M.fromList
   , ("-", VNativeFunc subtraction)
   , ("*", VNativeFunc multiplication)
   , ("/", VNativeFunc division)
+  , ("print", VNativeFunc print')
   ]
 
 addition :: [Value] -> Maybe Value
@@ -16,7 +18,7 @@ addition [VInt x, VInt y] = Just $ VInt (x + y)
 addition _                = Nothing
 
 subtraction :: [Value] -> Maybe Value
-subtraction [VInt x] = Just $ VInt (-x)
+subtraction [VInt x]         = Just $ VInt (-x)
 subtraction [VInt x, VInt y] = Just $ VInt (x - y)
 subtraction _                = Nothing
 
@@ -27,3 +29,7 @@ multiplication _                = Nothing
 division :: [Value] -> Maybe Value
 division [VInt x, VInt y] = Just $ VInt (x `div` y)
 division _                = Nothing
+
+print' :: [Value] -> Maybe Value
+print' [x] = Just $ trace (show x) x
+print' _   = Nothing
