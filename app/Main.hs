b3814d1f832fb79a902933eb8def157002b7095e
kintrix007 <boldizsar02@gmail.com>
2023-08-02 10:43:07 +0200

    feat: Added CLI to run a script

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,23 @@
 module Main (main) where
 
+import           Data.Map           as M
+import           ExprParse          (runExprParser)
+import           System.Environment (getArgs)
+import Interpreter (interpret, Value (..))
+
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = do
+  [filePath] <- getArgs
+  sourceCode <- readFile filePath
+
+  let ast = case runExprParser sourceCode of
+        Left x  -> error x
+        Right x -> x
+
+  let res = case interpret M.empty ast of
+        Nothing -> error "Runtime error"
+        Just x -> x
+  
+  case res of
+    VInt x -> print x
+    VFunc _ x _ -> putStrLn ("(fn (" ++ unwords x ++ ") ?)")
